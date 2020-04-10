0e0c19b59523cce6605f074d47415750b59acd5d
JunDamin <freedomgod@gmail.com>
2020-04-10 21:24:46 +0430

    #1.3.  create master data model

--- data/models.py
+++ data/models.py
@@ -17,5 +17,10 @@ class Employment():
     name = models.CharField(max_length=255)
     koica_code = models.CharField(max_length=255)
     location = models.ForeignKey(Country, related_name='location', on_delete=models.PROTECT)
+    address = models.TextField()
     countries = models.ManyToManyField("Country", related_name="countries", blank=True)
 
+
+class Region():
+    name = models.CharField(max_length=255)
+    countries = models.ManyToManyField("Country", related_name="countries", blank=True)
