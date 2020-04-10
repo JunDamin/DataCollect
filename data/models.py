5eae5c02befe520e120cdd04e99c16447e2f3bf3
JunDamin <freedomgod@gmail.com>
2020-04-10 21:19:59 +0430

    #1.2. design  user model

--- data/models.py
+++ data/models.py
@@ -1,4 +1,21 @@
 from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
+
 
 # Create your models here.
+
+class Country():
+    code = CountryField()
+
+    def __str__(self):
+        return self.code.name
+
+
+class Employment():
+
+    name = models.CharField(max_length=255)
+    koica_code = models.CharField(max_length=255)
+    location = models.ForeignKey(Country, related_name='location', on_delete=models.PROTECT)
+    countries = models.ManyToManyField("Country", related_name="countries", blank=True)
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import AbstractUser
+from django.shortcuts import reverse
 from django.db import models
+from django_countries.fields import CountryField
+from data import models as data_models
 
 # Create your models here.
+
+
+class User(AbstractUser):
+
+    LOGIN_EMAIL = "email"
+    LOGIN_GITHUB = "github"
+    LOGIN_KAKAO = "kakao"
+
+    LOGIN_CHOICES = (
+        (LOGIN_EMAIL, ""),
+        (LOGIN_GITHUB, ""),
+        (LOGIN_KAKAO, ""),
+    )
+
+    avatar = models.ImageField(upload_to="avatars", blank=True)
+    bio = models.TextField(default="", blank=True)
+    employment = models.ForeignKey(
+        data_models.Employment,
+        related_name="employment",
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+    )
+
+    email_verified = models.BooleanField(default=False)
+    email_secret = models.CharField(max_length=64, default="", blank=True)
+    login_method = models.CharField(
+        max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
+    )
+
+    def get_absolute_url(self):
+        return reverse("users:profile", kwargs={"pk": self.pk})
