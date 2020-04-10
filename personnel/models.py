9f148f04bb5949f4d30c290538b02f028a61b2b8
JunDamin <freedomgod@gmail.com>
2020-04-10 22:41:03 +0430

    #1.5 create personnel model

--- personnel/models.py
+++ personnel/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
+from data import models as data_models
 
 # Create your models here.
+
+
+class PersonnelType(core_models.TimeStampedModel):
+
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    the_type = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class PersonnelInfo(core_models.TimeStampedModel):
+
+    employment = models.ForeignKey(data_models.Employment, related_name="personnel_info", on_delete=models.PROTECT)
+    personnel = models.ForeignKey(PersonnelType, related_name="personnel_info", on_delete=models.PROTECT)
+    number = models.IntegerField()
+    country = CountryField()
+    report_date = models.DateField()
+    
