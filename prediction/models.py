8bb9470b8bdb50896dd2c57d304e6e32a28621bd
JunDamin <freedomgod@gmail.com>
2020-04-10 22:47:35 +0430

    #1.6 Create prediction model

--- prediction/models.py
+++ prediction/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
+from data import models as data_models
 
-# Create your models here.
+
+class RiskType():
+    name = models.CharField(max_length=255)
+
+
+class RiskLevel():
+    name = models.CharField(max_length=255)
+
+
+class Prediction(core_models.TimeStampedModel):
+
+    employment = models.ForeignKey(data_models.Employment, related_name="prediction", on_delete=models.PROTECT)
+    country = CountryField()
+    report_date = models.DateField()
+    risk_type = models.ForeignKey(RiskType, related_name="prediction", on_delete=models.PROTECT)
+    risk_level = models.ForeignKey(RiskLevel, related_name="prediction", on_delete=models.PROTECT)
+    description = models.TextField()
+    action = models.TextField()
