cdfcec37dd2d014e667598505ea87c3f9c40f3be
JunDamin <freedomgod@gmail.com>
2020-04-19 22:53:24 +0430

    update setting

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+STATIC_ROOT = "/home/jundamin/jundamin.pythonanywhere.com/static"
 
 AUTH_USER_MODEL = "users.User"
