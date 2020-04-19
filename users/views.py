b5739e30431d20278c1b881312d4031acccd4ee2
JunDamin <freedomgod@gmail.com>
2020-04-19 21:56:10 +0430

    #4.6.8 user updates

--- users/views.py
+++ users/views.py
@@ -69,7 +69,7 @@ def complete_verification(request, key):
 
 def github_login(request):
     client_id = os.getenv("GH_ID")
-    redirect_uri = "http://jundamin.pythonanywhere.com/users/login/github/callback"
+    redirect_uri = "http://127.0.0.1:8000/users/login/github/callback"
     return redirect(
         f"https:github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user"
     )
@@ -143,7 +143,7 @@ def github_callback(request):
 
 def kakao_login(request):
     client_id = os.getenv("KAKAO_ID")
-    redirect_uri = "http://jundamin.pythonanywhere.com/users/login/kakao/callback"
+    redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
     return redirect(
         f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
     )
